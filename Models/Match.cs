15b9a3612ec5dea002949f8384e8dd9d6ce3e0e6
GABRIEL CANI <gabriel.cani@itiscuneo.edu.it>
2025-05-20 10:35:39 +0200

    Aggiunta Model,View,Controller partite

---
+++ Models/Match.cs
@@ -0,0 +1,14 @@
+﻿namespace ApexVolley.Models
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public string Avversari { get; set; }
+        public string Luogo { get; set; }
+        public string Risultato { get; set; }
+        public string RisultatoSet1 { get; set; }
+        public string RisultatoSet2 { get; set; }
+        public string RisultatoSet3 { get; set; }
+    }
+}
